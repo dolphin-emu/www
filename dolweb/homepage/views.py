47a454bd14e3cde47707e79425ad498e6f1686c0
Pierre Bourdon <delroth@gmail.com>
2012-08-28 13:40:27 +0200

    Added some screenshots on the homepage

--- dolweb/homepage/views.py
+++ dolweb/homepage/views.py
@@ -1,9 +1,9 @@
 from annoying.decorators import render_to
-from django.template import RequestContext
+from dolweb.media.models import Screenshot
 
 @render_to('homepage-home.html')
 def home(request):
-    return {}
+    return { 'featured_images': Screenshot.objects.filter(promoted=True).order_by('game_name') }
 
 def news_article(request):
     raise NotImplemented
