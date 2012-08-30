2941e369d460903876ff25bcf94f8cf73d650739
Pierre Bourdon <delroth@gmail.com>
2012-08-30 05:07:33 +0200

    More homepage text

--- dolweb/homepage/views.py
+++ dolweb/homepage/views.py
@@ -1,9 +1,15 @@
 from annoying.decorators import render_to
+from dolweb.downloads.models import ReleaseVersion
 from dolweb.media.models import Screenshot
 
 @render_to('homepage-home.html')
 def home(request):
-    return { 'featured_images': Screenshot.objects.filter(promoted=True).order_by('game_name') }
+    featured = Screenshot.objects.filter(promoted=True).order_by('game_name')
+    try:
+        last_release = ReleaseVersion.objects.order_by('-date')[0]
+    except IndexError:
+        last_release = u"Dolphin"
+    return { 'featured_images': featured, 'last_release': last_release }
 
 def news_article(request):
     raise NotImplemented
