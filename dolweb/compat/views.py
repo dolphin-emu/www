c39f557275d14cc329badf2d4d228606d3c79cde
Pierre Bourdon <delroth@gmail.com>
2013-01-29 22:17:53 +0100

    Removed some print statements left in the code

--- dolweb/compat/views.py
+++ dolweb/compat/views.py
@@ -17,9 +17,7 @@ def list_compat(request, first_char='#'):
                                 latest__text__data_raw__in=('1', '2', '3', '4', '5'))
                         .exclude(title_url='Ratings/'))
     if first_char == '#':
-        print len(vers)
         vers = vers.filter(title_url__iregex=r'^Ratings/[^a-zA-Z].*$')
-        print len(vers)
 
     vers = vers.select_related('latest__text__data_raw', 'latest__timestamp_raw').order_by('title_url')
 
