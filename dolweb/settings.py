ef8b8484f522603305a31b6d99b49a17c984e2f6
Pierre Bourdon <delroth@gmail.com>
2013-02-20 15:02:59 +0100

    Add a dyni18n_faq command to automatically download FAQ translations from Transifex

--- dolweb/settings.py
+++ dolweb/settings.py
@@ -233,6 +233,12 @@ BNR_MONGO_PASSWORD = ''
 # Path used to load and update dynamic i18n PO files
 DYNI18N_PATH = os.path.join(PROJECT_ROOT, 'dyni18n')
 
+# Transifex informations for automatic dyni18n pull
+TRANSIFEX_USER = ''
+TRANSIFEX_PASSWORD = ''
+TRANSIFEX_PROJECT = ''
+TRANSIFEX_FAQ_RESOURCE = ''
+
 try:
     execfile(os.path.join(PROJECT_ROOT, 'dolweb', 'local_settings.py'), globals(), locals())
 except ImportError:
