7e8bc7dd7fa7e1b35054a2655224728941317d3d
Alexandre `Zopieux` Macabies <web@zopieux.com>
2013-09-30 17:30:16 +0200

    Woops forget feeds.py

---
+++ dolweb/blog/feeds.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.utils.translation import ugettext as _
+from models import BlogSerie
+from zinnia.feeds import EntryFeed
+
+
+class SerieFeed(EntryFeed):
+    def get_object(self, request, pk):
+        serie = get_object_or_404(BlogSerie, pk=pk)
+        if not serie.visible:
+            raise Http404
+        return serie
+
+    def items(self, obj):
+        return obj.entries.all()[:settings.ZINNIA_FEEDS_MAX_ITEMS]
+
+    def link(self, obj):
+        return reverse('dolweb.blog.views.series_index', args=[obj.pk])
+
+    def get_title(self, obj):
+        return _("Entries for the serie %s") % obj.name
+
+    def description(self, obj):
+        return _("The latest entries for the serie %s") % obj.name
