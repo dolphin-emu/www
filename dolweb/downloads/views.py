0547bc232ebd028d394bdc3b93d3adda753c347f
Jules Blok <jules.blok@gmail.com>
2015-11-02 00:22:49 +0100

    Revert "Add RC to downloads list."
    This reverts commit 6cf51c5ee558661995e0d07c2cdc5ef38f7efc0b.

--- dolweb/downloads/views.py
+++ dolweb/downloads/views.py
@@ -18,12 +18,11 @@ def index(request):
     """Displays the downloads index"""
 
     releases = ReleaseVersion.objects.order_by('-date')
-    master_builds = DevVersion.objects.filter(branch='master').order_by('-date')[:5]
-    stable_builds = DevVersion.objects.filter(branch='stable').filter(shortrev__istartswith='5.0-rc').order_by('-date')[:3]
+    master_builds = DevVersion.objects.filter(branch='master').order_by('-date')[:10]
     last_master = master_builds[0] if len(master_builds) else None
 
     return { 'releases': releases, 'master_builds': master_builds,
-             'last_master': last_master, 'stable_builds': stable_builds }
+             'last_master': last_master }
 
 @render_to('downloads-branches.html')
 def branches(request):
