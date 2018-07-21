a5ef94d0cc3ea9ef7dfd18ba5cdc8072ed9618b8
Pierre Bourdon <delroth@gmail.com>
2018-07-21 22:51:38 +0200

    dolweb: add media context processor to template settings

--- dolweb/settings.py
+++ dolweb/settings.py
@@ -147,6 +147,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
+                'django.template.context_processors.media',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
