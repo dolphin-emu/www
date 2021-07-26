9084fc62bacfb16553651e3cab9e6e5354c4a105
Ryan Woodward <smashocracy@gmail.com>
2021-07-26 12:57:38 -0700

    downloads: fix inconsistent order for macOS download links
    Sort ARM/Intel Universal builds before Intel builds, and also change
    historical Mac OS X builds to have the same position as modern macOS
    builds.

--- dolweb/downloads/templatetags/artifacts.py
+++ dolweb/downloads/templatetags/artifacts.py
@@ -11,10 +11,12 @@ def artifact_sort(artifacts):
     matchers = (
         ('Windows', 0x8000),
         ('macOS', 0x4000),
+        ('Mac OS X', 0x3ff0), # for historical builds
         ('Android', 0x2000),
         ('Ubuntu', 0x1000),
         ('x64', 0x8),
         ('x86', 0x4),
+        ('Universal', 0x2),
     )
     def key(artifact):
         k = 0
