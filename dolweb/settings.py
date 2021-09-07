589d01a06ca5a01ab8a2c524c5acdb0d10948f63
Léo Lam <leo@leolam.fr>
2021-09-07 14:07:56 +0200

    settings: fix Twitter cards by not using protocol relative URLs

--- dolweb/settings.py
+++ dolweb/settings.py
@@ -110,8 +110,10 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media', 'user')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
+# Do not use protocol-relative URLs because some crawlers (e.g. Twitter's)
+# consider them invalid.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '//dolphin-emu.org/m/user/'
+MEDIA_URL = 'https://dolphin-emu.org/m/user/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -121,7 +123,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'media', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '//dolphin-emu.org/m/static/'
+STATIC_URL = 'https://dolphin-emu.org/m/static/'
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
