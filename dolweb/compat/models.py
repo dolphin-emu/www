79c6eb18aa8c5c48d21ca87fb4f57edc35e10bef
Pierre Bourdon <delroth@gmail.com>
2023-01-10 03:48:22 +0100

    compat: work around new DB format with a SQL view

--- dolweb/compat/models.py
+++ dolweb/compat/models.py
@@ -49,10 +49,10 @@ class Revision(models.Model):
     timestamp = models.CharField(db_column='rev_timestamp', max_length=14)
 
     def __str__(self):
-        return '%s for %s' % (self.timestamp_raw, self.page)
+        return '%s for %s' % (self.timestamp, self.page)
 
     class Meta:
-        db_table = 'revision'
+        db_table = 'old_revision'
         verbose_name = 'MediaWiki Revision'
         verbose_name_plural = 'MediaWiki Revisions'
 
