995a897a19ee451415756973d5991675083943e1
Pierre Bourdon <delroth@gmail.com>
2023-01-10 03:49:34 +0100

    compat: re-enable

--- dolweb/compat/urls.py
+++ dolweb/compat/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import url
 from dolweb.compat.views import list_compat
 
 urlpatterns = [
-    #url(r'^$', list_compat, name='compat_index'),
-    #url(r'^(?:(?P<first_char>[A-Z#])/)?(?:filter/(?P<filter_by>[12345])/)?$', list_compat, name='compat_list'),
+    url(r'^$', list_compat, name='compat_index'),
+    url(r'^(?:(?P<first_char>[A-Z#])/)?(?:filter/(?P<filter_by>[12345])/)?$', list_compat, name='compat_list'),
 ]
