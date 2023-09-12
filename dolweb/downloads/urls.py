2678641e1e9030920862b2eaecafe5cb3e54564b
OatmealDome <julian@oatmealdome.me>
2023-09-12 19:13:44 -0400

    downloads: Allow periods to be used in branch names

--- dolweb/downloads/urls.py
+++ dolweb/downloads/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     url(r'^branches/$', views.branches, name='downloads_branches'),
     url(r'^new/$', views.new, name='downloads_new'),
 
-    url(r'list/(?P<branch>[a-zA-Z0-9_-]+)/(?:(?P<page>\d+)/)?$', views.list, name='downloads_list'),
+    url(r'list/(?P<branch>[a-zA-Z0-9._-]+)/(?:(?P<page>\d+)/)?$', views.list, name='downloads_list'),
 
     url(r'^dev/(?P<hash>[0-9a-f]{40})/$', views.view_dev_release,
         name='downloads_view_devrel'),
     url(r'^dev/(?P<branch>.*?)/(?P<name>.*?)/$', views.view_dev_release_by_name,
         name='downloads_view_devrel_by_name'),
 
-    url(r'^latest/(?P<branch>[a-zA-Z0-9_-]+)/$', views.get_latest, name='downloads_get_latest'),
+    url(r'^latest/(?P<branch>[a-zA-Z0-9._-]+)/$', views.get_latest, name='downloads_get_latest'),
     url(r'^buildlist$', views.buildlist, name='buildlist_index'),
 ]
